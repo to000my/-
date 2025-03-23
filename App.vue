aad6f32bb3558383581e86701a3e47f57a833e14
to000my <819850142@qq.com>
2025-03-23 21:18:11 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-container">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// App.vue作为根组件，负责应用的整体布局
+</script>
+
+<style lang="scss">
+.app-container {
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+}
+</style>
